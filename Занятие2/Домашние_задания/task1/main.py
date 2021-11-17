e1f9b8c68cf23723ed38612eb0fc8461ec830515
AleUli <alek-ulitin@yandex.ru>
2021-11-17 21:04:34 +0300

    init

--- Занятие2/Домашние_задания/task1/main.py
+++ Занятие2/Домашние_задания/task1/main.py
@@ -1,20 +1,28 @@
 from itertools import count
 def generator_geometry(base,count_):
-    n = 0
     while True:
-        yield base * count_
+        result = base * count_
+        yield result
+        result1 = (result * count_ - base) / (count_ - 1)
+        yield result1
+
+
 
 if __name__ == "__main__":
     # Write your solution here
     pass
+)
 
 
 
 
 
 
-
-
+    # while True:
+    #     result = base * count_
+    #     yield result
+    #     result1 = (result * count_ - base) / (count_ - 1)
+    #     yield result1
 
 
 # def pow_gen(base: int):
